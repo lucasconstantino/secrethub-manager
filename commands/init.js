a928c749a095a141d233b2fe4b025b32f17922d0
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-12-01 21:00:35 +0100

    refactor: moved all credential logic to lib

--- commands/init.js
+++ commands/init.js
@@ -1,30 +1,13 @@
 const { execSync } = require("child_process");
 const { Command } = require("commander");
 const { prompt } = require("enquirer");
-
-const readCredentials = require("../lib/credential");
 const config = require("../lib/config");
-
-const { SECRETHUB_CREDENTIAL } = process.env;
-
-const prompts = {
-  credential: {
-    type: "password",
-    name: "credential",
-    required: true,
-    message: "No secrethub credential found. Please, provide one:",
-  },
-};
+const credential = require("../lib/credential");
 
 module.exports = new Command()
   .command("init")
   .description("Setup .env file from a template")
   .action(async () => {
-    const credential =
-      SECRETHUB_CREDENTIAL ||
-      readCredentials() ||
-      (await prompt(prompts.credential));
-
     const variables = [];
 
     for (const [name, variable] of Object.entries(config.variables)) {
@@ -39,7 +22,7 @@ module.exports = new Command()
     }
 
     execSync(
-      `SECRETHUB_CREDENTIAL=${credential} secrethub inject --no-prompt ${variables.join(
+      `SECRETHUB_CREDENTIAL=${await credential()} secrethub inject --no-prompt ${variables.join(
         " "
       )} -i secrethub.env > ./.env`,
       { stdio: "inherit" }
